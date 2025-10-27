6758d717fd2167015093a6ac87e0fba08a88d1fa
MKato2361 <kojirou318jes@gmail.com>
2025-10-27 20:23:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -410,7 +410,7 @@ with tabs[1]:
         st.info("先に「1. ファイルのアップロード」タブでExcelファイルをアップロードすると、イベント登録機能が利用可能になります。")
 
     st.subheader("➡️ イベント登録・更新実行")
-        if st.button("Googleカレンダーに登録・更新する"):
+    if st.button("Googleカレンダーに登録・更新する"):
                 set_user_setting(user_id, 'description_columns_selected', description_columns)
                 set_user_setting(user_id, 'event_name_col_selected', selected_event_name_col)
                 set_user_setting(user_id, 'add_task_type_to_event_name', add_task_type_to_event_name)
