3146cc8d9cbbef50cf935fec0f2889484043ec53
MKato2361 <kojirou318jes@gmail.com>
2026-04-30 17:46:48 +0900

    Update tab2_register.py

--- tabs/tab2_register.py
+++ tabs/tab2_register.py
@@ -548,7 +548,7 @@ def _execute_registration(
 
 def render_tab2_register(user_id: str, manager):
     """タブ2: イベント登録・更新"""
-    service = manager.calendar_service
+    service = st.session_state.get("calendar_service")
     editable_calendar_options = manager.editable_calendar_options
 
     work_files = st.session_state.get("uploaded_files") or []
